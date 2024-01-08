a9202c9d10e77aae8eda0597ffa0edad608554f1
Алёна <alyona.glm@gmail.com>
2024-01-08 23:20:54 +0300

    1 commit

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useFetchJson } from "./hooks/useFetchJson";
+
+export default function Error() {
+  const [data, loading, error] = useFetchJson("http://localhost:7070/error");
+  return (
+    <React.Fragment>
+      {error ? <div className="error">{error}</div> : null}
+    </React.Fragment>
+  );
+}
---
+++ src/components/hooks/useFetchJson.js
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+
+export const useFetchJson = (url, options = null) => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url,options);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const json = await response.json();
+      setData(json);
+      setError(null);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return [data, loading, error] ;
+};
